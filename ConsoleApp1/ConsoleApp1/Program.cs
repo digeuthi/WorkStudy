a4fc4db0109b2d1fef596db821718e124d61533f
Dahye Kim <digeuthi@gmail.com>
2024-07-06 15:31:52 +0900

    console ver application

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+
+
+class Program {
+static SerialPort serialPort;
+    static void Main(string[] args) {
+
+        Console.WriteLine("Available COM Port : ");
+        foreach (string s in SerialPort.GetPortNames()) {
+        Console.WriteLine(" " + s);
+        }
+
+        Console.Write("Enter COM port value : ");
+        string portName = Console.ReadLine();
+
+        serialPort = new SerialPort(portName)
+        {
+            BaudRate = 115200,
+            DataBits = 8,
+            StopBits = StopBits.One,
+            Parity = Parity.None
+        };
+
+        serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+        try {
+            serialPort.Open();
+            Console.WriteLine($"Serial port {portName} opend");
+        } catch (Exception e) {
+            Console.WriteLine($"Failed to open serial port {portName} : ");
+            return;
+        }
+
+        Console.WriteLine("Press any key to close the application");
+        Console.ReadKey();
+
+        if (serialPort.IsOpen) {
+            serialPort.Close();
+            Console.WriteLine("Serial port closed");
+        }
+
+    }
+    private static void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+    {
+        try
+        {
+            string data = serialPort.ReadExisting();
+            Console.WriteLine($"Data received: {data}");
+        }
+        catch (InvalidOperationException ex)
+        {
+            Console.WriteLine($"Invalid operation while reading: {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error while reading data: {ex.Message}");
+        }
+    }
+}
