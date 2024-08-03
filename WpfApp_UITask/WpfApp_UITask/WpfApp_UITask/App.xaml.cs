fc719eedc396bc164c032a28266c417d98b8497d
Dahye Kim <digeuthi@gmail.com>
2024-08-03 17:12:41 +0900

    UI task format

---
+++ WpfApp_UITask/WpfApp_UITask/WpfApp_UITask/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfApp_UITask
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
